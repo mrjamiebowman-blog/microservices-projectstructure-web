d222cd631d14ad51d9f3b897b8bcf46c44699046
Jamie Bowman <mrjamiebowman@gmail.com>
2024-02-12 19:53:28 -0600

    updates

--- src/MrJB.Solution.Web/Program.cs
+++ src/MrJB.Solution.Web/Program.cs
@@ -1,10 +1,11 @@
 using MrJB.Solution.Web;
 using MrJB.Solution.Web.Components;
+using MrJB.Solution.Web.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add service defaults & Aspire components.
-builder.AddServiceDefaults();
+//builder.AddServiceDefaults();
 builder.AddRedisOutputCache("cache");
 
 // Add services to the container.
